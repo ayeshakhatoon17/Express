2c9288f8ff725f26de3323ebc723e2c865b82325
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-03-30 07:32:35 +0000

    req.hostname

--- hostname.js
+++ hostname.js
@@ -4,7 +4,7 @@ const PORT = 5000;
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.status(200).send(req.hostname)//shows host's ip : 192.168.0.176
+    res.status(200).send(req.hostname)//shows host's ip address: 192.168.0.176
 })
 
 app.listen(PORT,()=>{
