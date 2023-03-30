e96c00d8b77351e181d12c59feffedc418214493
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-03-30 08:07:51 +0000

    req.body

--- body.js
+++ body.js
@@ -3,8 +3,9 @@ import express from 'express';
 const PORT = 5000;
 const app = express();
 
-app.get('/',(req,res)=>{
+app.post('/',(req,res)=>{
     res.send(req.body)
+    console.log(req.body)
 })
 
 app.listen(PORT,()=>{ 
