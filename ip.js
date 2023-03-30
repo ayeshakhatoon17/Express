dcef708efd5477bb333c0a826452bfd902c0c2aa
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-03-30 07:38:04 +0000

    req.ip

--- ip.js
+++ ip.js
@@ -4,7 +4,7 @@ const PORT = 5000;
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.status(200).send(req.ip) //::ffff:192.168.0.140 shows the ip of requested one's
+    res.status(200).send(req.ip) //::ffff:192.168.0.140 shows the ip of the person who accessed 
 })
 
 app.listen(PORT,()=>{
