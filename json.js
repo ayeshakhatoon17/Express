d870d811853f98cc738497300d03dfd4989c6c81
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-03-30 07:38:52 +0000

    res.json

--- json.js
+++ json.js
@@ -4,7 +4,7 @@ const PORT = 5000;
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.status(200).json({name : 'ayesha'})
+    res.status(200).json({name : 'ayesha'}) //prints objects in json format
 })
 
 app.listen(PORT,()=>{
