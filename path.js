f8032bb15ce9878fd0a674004abf838717331df5
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-03-30 07:42:25 +0000

    req.path

--- path.js
+++ path.js
@@ -3,10 +3,11 @@ import express from 'express';
 const PORT = 5000;
 const app = express();
 
-app.get('/',(req,res)=>{
-    
+app.get('/path',(req,res)=>{
+    res.send(req.path)
 })
 
 app.listen(PORT,()=>{
     console.log('server listening at port',PORT) 
 })
+//path shows the inserted after /
