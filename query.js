9b4aeae000432b4a4f89c5fa6e08d6450af802a1
ayeshakhatoon17 <ayeshakhatoon02.ak@gmail.com>
2023-03-30 08:09:46 +0000

    req.query

--- query.js
+++ query.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send(req.query) //joh query params me key and value dere post name woh return karta 
-    console.log(req.query)////joh query params me key and value dere post name woh return karta terminal peh
+    console.log(req.query)//joh query params me key and value dere post name woh return karta terminal peh
 })
 
 app.listen(PORT,()=>{
