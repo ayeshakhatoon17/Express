96da6dfacae5c352c1685a0e6f14dfb90133b707
Ayesha Khatoon <ayeshakhatoon02.ak@gmail.com>
2023-08-19 21:18:07 +0530

    Update body.js

--- body.js
+++ body.js
@@ -1,14 +1,1 @@
-import express from 'express';
 
-const PORT = 5000;
-const app = express();
-
-app.post('/',(req,res)=>{
-    res.send(req.body)
-    console.log(req.body)
-})
-
-app.listen(PORT,()=>{ 
-    console.log('server listening at port',PORT)
-   
-})
